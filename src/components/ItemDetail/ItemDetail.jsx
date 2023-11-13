1b078633e0c6af4955c5d5beb6be72e6ac6b9d57
DARP-MAKER <ingenierodiegorios@gmail.com>
2023-11-12 23:51:34 -0500

    carga actualizada 12-11-2023

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -5,7 +5,7 @@ const ItemDetail = ({itemSelected}) => {
     <div>
         <h6 className="card-title">{itemSelected?.title}</h6>
         <img src={itemSelected?.image} alt={itemSelected?.title} width={70} />
-        <div> className="card-description"
+        <div className="card-description">
             <p>{itemSelected?.description}</p>
         </div>
 
--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,24 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ItemDetail from "../ItemDetail/ItemDetail";
 import { useParams } from "react-router-dom";
 
 const ItemDetailContainer = () => {
+    const [product, setProduct] = useState({});
     const { id } = useParams();
     
     const fetchProducts = () => {
         fetch(`https://fakestoreapi.com/products/${id}`)
         .then((response) => response.json())
-        .then((data) => console.log(data))
+        .then((data) => {
+            console.log(data);
+            setProduct(data);
+        })
         .catch((error) => console.log(error));
     };
 
     useEffect(() => {
         fetchProducts();
-    }, []);
+    }, [id]);
 
     return (
         <div>
-            <ItemDetail />
+            <ItemDetail itemSelected={product}/>
         </div>
     );
 };
